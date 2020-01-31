6804a2a3e667c1d3d2355a8ceb9bf98e1fba783c
Barak Merdler <barakmerdler@gmail.com>
2020-01-31 12:33:56 +0200

    build directory

---
+++ client/build/precache-manifest.9338268dadfe5fb4907f117d1a9768c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f727f1d329ce0b2b5aa26bcebf86f36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfc32c2d1260b8212b0a",
+    "url": "/static/css/main.f04ef468.chunk.css"
+  },
+  {
+    "revision": "e1071c98aefedb5fc995",
+    "url": "/static/js/2.f32f1b19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f32f1b19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfc32c2d1260b8212b0a",
+    "url": "/static/js/main.b7838c87.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
